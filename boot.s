6a2cbfb2e168aa0869cdc5a095942bdffe26c375
macedonianking <lhb198710@gmail.com>
2014-06-22 17:08:19 +0800

    print load message to out

--- boot.s
+++ boot.s
@@ -1,14 +1,44 @@
 	.code16
-	.data
+	.text
 	.globl	main
-	.org	0x00
+	.org	0x0000
 main:
-	mov		$'a', %al
-	mov		$0x0e,%ah
-	mov		$15, %bx
-	int		$0x10
+	mov		$0x00, %ax
+	mov		%ax, %ss
+	mov		%ax, %ds
+	mov		%ax, %es
+	mov		%ax, %bp
+	mov		$main, %sp
+
+	call	PRINT_MSG
+
+FINISH:
 	hlt	
-	jmp		main
+	jmp		FINISH
+
+PRINT_MSG:
+	push	%bp
+	mov		%sp, %bp
+
+	mov		$MSG, %di
+PRINT_L1:
+	mov		(%di), %al
+	cmp		$0x00, %al
+	jz		PRINT_L2
+	push	%di
+	mov		$0x0e, %ah
+	mov		$0xff, %bx
+	int		$0x10
+	pop		%di
+	inc		%di
+	jmp		PRINT_L1
+
+PRINT_L2:
+	mov		%bp, %sp
+	pop		%sp
+	ret
+MSG:
+	.string	"I am loading...\r\n"
 	.org	0x1fe
 	.short	0xaa55
 
